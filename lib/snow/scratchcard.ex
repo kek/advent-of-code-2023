ac533399b57ba44321e1131587fcfc43771dc4d0
Karl Eklund <localpart@gmail.com>
2023-12-05 01:03:07 +0100

    day 4 part two

--- lib/snow/scratchcard.ex
+++ lib/snow/scratchcard.ex
@@ -10,11 +10,45 @@ defmodule Snow.Scratchcard do
   end
 
   def pile_worth([card | rest]) do
-    wins = MapSet.intersection(card.left, card.right) |> MapSet.size()
+    wins = wins(card)
 
-    case wins do
+    score(wins) + pile_worth(rest)
+  end
+
+  defp score(x) do
+    case x do
       0 -> 0
       something -> Integer.pow(2, something - 1)
-    end + pile_worth(rest)
+    end
+  end
+
+  def win_more(%{id: id} = root, deck) do
+    case wins(root) do
+      0 ->
+        []
+
+      score ->
+        (id + 1)..(id + 1 + score - 1)
+        |> Enum.map(&get_card_by_id(&1, deck))
+        |> Enum.map(fn won_card ->
+          Map.put_new(won_card, :id, id)
+        end)
+        |> Enum.flat_map(fn won_card ->
+          [root | win_more(won_card, deck)]
+        end)
+    end
+  end
+
+  defp wins(card) do
+    MapSet.intersection(card.left, card.right) |> MapSet.size()
+  end
+
+  defp get_card_by_id(id, deck) do
+    Enum.find(deck, :not_found, &(&1.id == id))
+    |> tap(fn found? ->
+      if found? == :not_found do
+        raise "Card ID not found: #{inspect(id)}"
+      end
+    end)
   end
 end
--- test/snow/days/day4_test.exs
+++ test/snow/days/day4_test.exs
@@ -62,4 +62,51 @@ defmodule Snow.Days.Day4Test do
     cards = Snow.Scratchcard.from_parsed(parsed)
     assert Snow.Scratchcard.pile_worth(cards) == 18619
   end
+
+  test "part 2 example" do
+    {:ok, parsed, _, _, _, _} = Snow.Scratchcard.Parser.scratchcard(@example)
+    cards = Snow.Scratchcard.from_parsed(parsed)
+    _win_more_cards = cards |> Enum.flat_map(&Snow.Scratchcard.win_more(&1, cards))
+  end
+
+  @example """
+  Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+  Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+  Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+  Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+  Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+  Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+  """
+
+  test "example" do
+    {:ok, parsed, _, _, _, _} = Snow.Scratchcard.Parser.scratchcard(@example)
+
+    deck =
+      Snow.Scratchcard.from_parsed(parsed)
+
+    game = deck ++ Enum.flat_map(deck, &Snow.Scratchcard.win_more(&1, deck))
+    assert Enum.count(game) == 30
+  end
+
+  @real_input File.read!("priv/input/Day 4 input.txt")
+
+  test "part two real data" do
+    {:ok, parsed, _, _, _, _} = Snow.Scratchcard.Parser.scratchcard(@real_input)
+
+    deck =
+      Snow.Scratchcard.from_parsed(parsed)
+
+    game = deck ++ Enum.flat_map(deck, &Snow.Scratchcard.win_more(&1, deck))
+    assert Enum.count(game) == 8_063_216
+  end
+
+  def human(cards) do
+    cards
+    |> Enum.map(& &1.id)
+    |> Enum.sort()
+    |> Enum.chunk_by(& &1)
+    |> Enum.map(fn [id | _] = cards ->
+      [id, Enum.count(cards)]
+    end)
+  end
 end
