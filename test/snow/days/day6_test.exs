33f9d3e35fc76220ec25be39e31b6eea4a7d7c8a
Karl Eklund <localpart@gmail.com>
2023-12-06 17:45:42 +0100

    Refactor

--- test/snow/days/day6_test.exs
+++ test/snow/days/day6_test.exs
@@ -21,25 +21,6 @@ defmodule Snow.Days.Day6Test do
     # Time, record_distance
     races = [{7, 9}, {15, 40}, {30, 200}]
     assert races |> Enum.map(ways_to_win) |> Enum.reduce(&Kernel.*/2) == 288
-  end
-
-  test "part one real data" do
-    distance = fn hold_time, race_duration ->
-      travel_time = race_duration - hold_time
-      speed = hold_time
-      distance = speed * travel_time
-      distance
-    end
-
-    ways_to_win = fn {race_duration, record_distance} ->
-      0..race_duration
-      |> Enum.count(fn hold_time ->
-        distance.(hold_time, race_duration) > record_distance
-      end)
-    end
-
-    # Time, record_distance
-    races = @my_input
-    assert races |> Enum.map(ways_to_win) |> Enum.reduce(&Kernel.*/2) == 160_816
+    assert @my_input |> Enum.map(ways_to_win) |> Enum.reduce(&Kernel.*/2) == 160_816
   end
 end
