5f4f24a7b368ff41a3091c98f33a43e0aa444158
Karl Eklund <localpart@gmail.com>
2023-12-11 02:16:27 +0100

    make images

--- lib/snow_web/controllers/page_controller.ex
+++ lib/snow_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule SnowWeb.PageController do
   use SnowWeb, :controller
 
   def home(conn, _params) do
+    {:ok, image} = Image.new(100, 100, color: :white)
+    {:ok, image} = Image.Draw.circle(image, 50, 50, 25, color: :black)
+    filename = "priv/static/images/circle.png"
+    File.rm(filename)
+    Image.write(image, filename)
     # The home page is often custom made,
     # so skip the default app layout.
     render(conn, :home, layout: false)
