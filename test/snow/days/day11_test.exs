07cd8d870e56b82bc43d6c8f79c7bf62c82705ac
Karl Eklund <localpart@gmail.com>
2023-12-12 23:40:59 +0100

    How to run code

---
+++ test/snow/days/day11_test.exs
@@ -0,0 +1,3 @@
+defmodule Snow.Days.Day11Test do
+  # {:ok, universe, _, _, _, _} = Snow.Cosmos.Universe.document(Snow.Cosmos.Examples.my_input()); {time, _} = :timer.tc fn -> Snow.Cosmos.Universe.expand(universe, 10000000) end; time
+end
