a8dcdac44d34fad841f4312fa62c0f62befd5a48
Karl Eklund <localpart@gmail.com>
2023-12-12 21:27:39 +0100

    start part two

--- lib/snow/cosmos/universe.ex
+++ lib/snow/cosmos/universe.ex
@@ -24,7 +24,6 @@ defmodule Snow.Cosmos.Universe do
     )
   )
 
-  @spec get(list(list()), {x :: integer(), y :: integer()}) :: :space | :galaxy
   def get(space_map, {x, y}) do
     case Enum.at(space_map, y) do
       nil -> nil
@@ -32,25 +31,28 @@ defmodule Snow.Cosmos.Universe do
     end
   end
 
-  @spec expand(nonempty_maybe_improper_list()) :: list()
-  def expand(universe) do
+  def get_v(space_map, {x, y}) do
+    case Enum.at(space_map, y) do
+      nil -> nil
+      row -> Enum.at(row, x)
+    end
+  end
+
+  def expand(universe, factor \\ 2) do
     universe
-    |> insert_rows()
     |> transpose()
-    |> insert_rows()
+    |> insert_rows(factor)
     |> transpose()
+    |> insert_rows(factor)
     |> contemplate_the_stars()
   end
 
-  def insert_rows(rows) do
+  def insert_rows(rows, factor) do
     Logger.debug("insert rows")
 
     Enum.flat_map(rows, fn row ->
-      n = Enum.count(row)
-
       if Enum.all?(row, &(&1 == :space)) do
-        added = Stream.cycle([row]) |> Enum.take(1)
-        [row | added] |> IO.inspect(label: "added")
+        Stream.cycle([row]) |> Enum.take(factor)
       else
         [row]
       end
@@ -58,29 +60,41 @@ defmodule Snow.Cosmos.Universe do
   end
 
   def transpose(rows) do
+    Logger.debug("transpose")
+
     rows
     |> List.zip()
     |> Enum.map(&Tuple.to_list/1)
   end
 
   def contemplate_the_stars(universe) do
+    Logger.debug("Contemplating the stars")
+    IO.inspect(universe, label: "decorated universe")
     height = Enum.count(universe)
     width = Enum.count(hd(universe))
 
+    Logger.debug(
+      "The universe is #{width} wide and #{height} high... That makes #{width * height} possibilities."
+    )
+
     stars =
       for x <- 0..(width - 1), y <- 0..(height - 1) do
-        case get(universe, {x, y}) do
+        case get_v(universe, {x, y}) do
           :space -> []
           :galaxy -> [{x, y}]
         end
       end
       |> Enum.flat_map(& &1)
 
+    Logger.debug("Finding the pairs")
+
     pairs =
       for a <- stars, b <- stars, a != b do
         MapSet.new([a, b])
       end
       |> Enum.uniq()
+
+    Logger.debug("Calculating the distances")
 
     for pair <- pairs do
       [{ax, ay}, {bx, by}] = Enum.take(pair, 2)
